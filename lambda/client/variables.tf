8aa93728a2cb02c52eb20b848c4bcae5caf86742
prabhukiran9999 <70853875+prabhukiran9999@users.noreply.github.com>
2021-09-23 18:43:57 -0400

    Create variables.tf

---
+++ lambda/client/variables.tf
@@ -0,0 +1,7 @@
+variable "target_env" {
+  description = "AWS workload account env (e.g. dev, test, prod, sandbox, unclass)"
+}
+
+variable "target_aws_account_id" {
+  description = "AWS workload account id"
+}
